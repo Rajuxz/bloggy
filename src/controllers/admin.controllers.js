14addc6e86a702c31a23947ff582eeeb01ffb936
Rajuxz <rajankhanalxz@gmail.com>
2025-07-09 22:42:59 +0545

    debug: password regex fixed

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -4,8 +4,8 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 
 const registerAdmin = asyncHandler(async (req, res) => {
     const { fullName, username, email, password } = req.body
-
-    // return res.status(200).json(new ApiResponse(200, { id: 1 }, "Hello there"))
+    console.log(fullName, username, email, password)
+    return res.status(200).json(new ApiResponse(200, { id: 1 }, "Hello there"))
 })
 
 export { registerAdmin }
