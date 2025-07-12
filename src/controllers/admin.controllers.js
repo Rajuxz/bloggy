e435c9a4bd55ceb4c627004b61f67b8875e3fdd6
Rajuxz <rajankhanalxz@gmail.com>
2025-07-12 17:51:17 +0530

    feat: admin register feature

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -1,11 +1,42 @@
 import { Admin } from "../models/admin.models.js"
 import asyncHandler from "../utils/asyncHandler.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
+import ApiError from "../utils/ApiError.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 const registerAdmin = asyncHandler(async (req, res) => {
     const { fullName, username, email, password } = req.body
-    console.log(fullName, username, email, password)
-    return res.status(200).json(new ApiResponse(200, { id: 1 }, "Hello there"))
+    //check if username or email already exists.
+    let userExists = await Admin.findOne({
+        $or: [{ username, email }],
+    })
+    if (userExists) {
+        throw new ApiError(409, "User already exists.")
+    }
+    const avatarLocalpath = req.files?.avatar[0]?.path
+    let avatar
+    if (avatarLocalpath) {
+        avatar = await uploadOnCloudinary(avatarLocalpath)
+    }
+
+    //create admin
+    const admin = await Admin.create({
+        fullName,
+        username: username.toLowerCase(),
+        email,
+        password,
+        avatar: avatar.url || "",
+    })
+    const createdAdmin = await Admin.findById(admin._id).select(
+        "-password -refreshToken"
+    )
+    if (!createdAdmin) {
+        throw new ApiError(500, "Something went wrong while saving user.")
+    }
+
+    return res
+        .status(201)
+        .json(new ApiResponse(200, createdAdmin, "User Created Successfully."))
 })
 
-export { registerAdmin }
+export default registerAdmin
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,10 +1,18 @@
 import { Router } from "express"
 const router = Router()
 
+import { upload } from "../middlewares/multer.middlewares.js"
 import validator from "../middlewares/validator.middleware.js"
-import { registerAdmin } from "../controllers/admin.controllers.js"
+import registerAdmin from "../controllers/admin.controllers.js"
 import { registerValidator } from "../validators/adminValidator.js"
 
-router.route("/register").post(registerValidator, validator, registerAdmin)
+router
+    .route("/register")
+    .post(
+        upload.fields([{ name: "avatar", maxCount: 1 }]),
+        registerValidator,
+        validator,
+        registerAdmin
+    )
 
 export default router
