39054655636811aa0130585af60facbb015cff7b
Rajuxz <rajankhanalxz@gmail.com>
2025-07-13 13:00:00 +0530

    mw: for verifying jwt

---
+++ src/middlewares/auth.middlewares.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken"
+import asyncHandler from "../utils/asyncHandler.js"
+import { Admin } from "../models/admin.models.js"
+import ApiError from "../utils/ApiError.js"
+
+export const verifyJwt = asyncHandler(async (req, _, next) => {
+    try {
+        const token =
+            req.cookies?.accessToken ||
+            req.header("Authorization")?.replace("Bearer ", "")
+        if (!token) {
+            throw new ApiError(401, "Unauthorized Access !")
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        //matches with user's _id attribute
+        const admin = await Admin.findById(decodedToken?._id).select(
+            "-password -refreshToken"
+        )
+
+        if (!admin) {
+            throw new ApiError(401, "Invalid Access Token !")
+        }
+        req.admin = admin
+        next()
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Internal Server Error")
+    }
+})
