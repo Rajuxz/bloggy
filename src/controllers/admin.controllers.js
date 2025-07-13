d756f450a8c769854303dff5dd38e5169c8a0dfb
Rajuxz <rajankhanalxz@gmail.com>
2025-07-13 12:59:36 +0530

    func: login and logout added

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -4,6 +4,11 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import ApiError from "../utils/ApiError.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
+const options = {
+    httpOnly: true,
+    secure: true,
+}
+
 const registerAdmin = asyncHandler(async (req, res) => {
     const { fullName, username, email, password } = req.body
     //check if username or email already exists.
@@ -39,4 +44,73 @@ const registerAdmin = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, createdAdmin, "User Created Successfully."))
 })
 
-export default registerAdmin
+const loginAdmin = asyncHandler(async (req, res) => {
+    const { identifier, password } = req.body
+    //find user
+    const admin = await Admin.findOne({
+        $or: [{ username: identifier }, { email: identifier }],
+    })
+
+    if (!admin) {
+        throw new ApiError(404, "User doesn't exists.")
+    }
+    const isPasswordValid = await admin.isPasswordCorrect(password)
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Incorrect Password.")
+    }
+    //get refresh and access token.
+    const { accessToken, refreshToken } = await getAccessAndRefreshToken(
+        admin._id
+    )
+    const authorizedAdmin = await Admin.findById(admin._id).select(
+        "-password -refreshToken"
+    )
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    loggedInAdmin: authorizedAdmin,
+                    accessToken,
+                    refreshToken,
+                },
+                "User Created Successfully"
+            )
+        )
+})
+
+const getAccessAndRefreshToken = async (id) => {
+    try {
+        const admin = await Admin.findById(id)
+
+        const accessToken = admin.generateAccessToken()
+        const refreshToken = admin.generateRefreshToken()
+
+        admin.refreshToken = refreshToken
+        await admin.save({ validateBeforeSave: false })
+        return { accessToken, refreshToken }
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Something went wrong.")
+    }
+}
+
+const logOutAdmin = asyncHandler(async (req, res) => {
+    await Admin.findById(
+        req.admin._id,
+        {
+            $set: { refreshToken: "" },
+        },
+        { new: true }
+    )
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User Logged Out"))
+})
+
+export { registerAdmin, loginAdmin, logOutAdmin }
