471f33ad98744524ed998b80caeb9f4bd0af04a2
Rajuxz <rajankhanalxz@gmail.com>
2025-07-31 23:17:46 +0545

    links for sidebar

---
+++ client/src/pages/admin/Links.jsx
@@ -0,0 +1,61 @@
+import {
+    Squares2X2Icon,
+    ArrowsRightLeftIcon,
+    BeakerIcon,
+    BugAntIcon,
+    Cog6ToothIcon,
+    QuestionMarkCircleIcon,
+    ArrowRightOnRectangleIcon,
+} from "@heroicons/react/24/outline"
+
+const iconClass = "h-4 w-4" // size similar to FontAwesome
+
+const SIDEBAR_ITEMS = [
+    {
+        key: "dashboard",
+        label: "Dashboard",
+        link: "/admin",
+        icon: <Squares2X2Icon className={iconClass} />,
+    },
+    {
+        key: "analytics",
+        label: "Analytics",
+        link: "analytics/",
+        icon: <ArrowsRightLeftIcon className={iconClass} />,
+    },
+    {
+        key: "manage",
+        label: "Manage",
+        link: "manage/",
+        icon: <BeakerIcon className={iconClass} />,
+    },
+    {
+        key: "bots",
+        label: "Bots",
+        link: "bots",
+        icon: <BugAntIcon className={iconClass} />,
+    },
+]
+
+const BOTTOM_LINKS = [
+    {
+        key: "settings",
+        label: "Settings",
+        link: "settings/",
+        icon: <Cog6ToothIcon className={iconClass} />,
+    },
+    {
+        key: "help",
+        label: "Help",
+        link: "help/",
+        icon: <QuestionMarkCircleIcon className={iconClass} />,
+    },
+    {
+        key: "logout",
+        label: "Log Out",
+        link: "logout/",
+        icon: <ArrowRightOnRectangleIcon className={iconClass} />,
+    },
+]
+
+export { SIDEBAR_ITEMS, BOTTOM_LINKS }
