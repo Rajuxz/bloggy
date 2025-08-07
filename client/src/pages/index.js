850db06cecc6a9bac6be9d5fcec6ba4b55684b06
Rajuxz <rajankhanalxz@gmail.com>
2025-08-07 21:35:39 +0545

    add: home, post , and addpost

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,5 @@
 import Dashboard from "./admin/Dashboard"
-
-export { Dashboard }
+import Home from "./admin/components/Home"
+import Post from "./admin/components/Post"
+import AddPost from "./admin/components/AddPost"
+export { Dashboard, Home, Post, AddPost }
