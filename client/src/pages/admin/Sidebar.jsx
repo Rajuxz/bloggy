f18712f1154d19cf8f08d5ef5a8c7e42120583d8
Rajuxz <rajankhanalxz@gmail.com>
2025-08-07 21:36:21 +0545

    modify: change color and icons

--- client/src/pages/admin/Sidebar.jsx
+++ client/src/pages/admin/Sidebar.jsx
@@ -7,21 +7,31 @@ const linkClasses =
 const Sidebar = ({ isOpen = true }) => {
     return (
         <div
-            className={`bg-white ${isOpen ? "w-60 h-screen transition-transform ease-in-out duration-300" : "hidden"} w-60 p-3 md:flex flex-col text-black `}
+            className={`bg-gray-300 ${isOpen ? "w-60 h-screen transition-transform ease-in-out duration-300" : "hidden"} w-60 p-3 md:flex flex-col text-black `}
         >
             <div className="flex-1">
                 <div className="flex items-center gap-2 px-1 py-2">
-                    <span className="text-base tracking-wide">Admin Area</span>
+                    <span className="text-base  tracking-wide">Admin Area</span>
                 </div>
                 <div className="pt-8 gap-0.5 flex flex-col flex-1 ">
                     {SIDEBAR_ITEMS.map((item) => {
-                        return <SidebarLink key={item.key} item={item} />
+                        return (
+                            <SidebarLink
+                                key={item.key}
+                                item={item}
+                            />
+                        )
                     })}
                 </div>
             </div>
             <div className="flex flex-col gap-0.5 pt-2 border-t border-t-black text-sm">
                 {BOTTOM_LINKS.map((item) => {
-                    return <SidebarLink key={item.key} item={item} />
+                    return (
+                        <SidebarLink
+                            key={item.key}
+                            item={item}
+                        />
+                    )
                 })}
             </div>
         </div>
