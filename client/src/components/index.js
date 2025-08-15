be2d5f27d3f7240e3fa8e5e43b192945facf596c
Rajuxz <rajankhanalxz@gmail.com>
2025-08-15 07:18:24 +0545

    add: export default for

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,5 @@
 import Navbar from "./Nav/Navbar"
 import Hero from "./Hero/Hero"
 import Sidebar from "./sidebar/Sidebar"
-export { Navbar, Hero, Sidebar }
+import Login from "./Login/Login"
+export { Navbar, Hero, Sidebar, Login }
